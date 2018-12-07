05f2e195d8868ce991cd5761c4d257517bb1a13f
kirzuev <kir2207@list.ru>
2018-12-07 13:44:36 +0300

    Update HW3

--- HW3/CloseByOne.hs
+++ HW3/CloseByOne.hs
@@ -1,15 +1,16 @@
 import Data.Matrix hiding (toList)
 import Data.Vector (toList)
-import Data.List (intersect, union, (\\), sort, nub)
+import Data.List (intersect, union, (\\), nub)
 
 inputMatrix :: Matrix Bool
 inputMatrix = fromLists
-  [ [  True, False, False, False, False ]
-  , [  True,  True, False,  True, False ]
-  , [  True,  True,  True,  True, False ]
-  , [ False, False,  True, False,  True ]
-  , [ False,  True, False,  True, False ]
-  , [ False, False,  True,  True,  True ]
+  --    a      b      c      d      e
+  [ [  True, False, False, False, False ] -- 1
+  , [  True,  True, False,  True, False ] -- 2
+  , [  True,  True,  True,  True, False ] -- 3
+  , [ False, False,  True, False,  True ] -- 4
+  , [ False,  True, False,  True, False ] -- 5
+  , [ False, False,  True,  True,  True ] -- 6
   ]
 
 down :: Matrix Bool -> [Int] -> [Int]
@@ -33,15 +34,15 @@ numToLetter i = snd $ head $
   filter (\(n,_) -> n == i) (zip [1,2..] ['a','b'..])
 
 closeByOne :: Matrix Bool -> [([Int], [Int])]
-closeByOne m = nub $ concat $
+closeByOne m = foldr union [] $
   (\g -> process m [g] g ((up m [g]), (down m $ up m [g]))) <$> [1..nrows m]
 
 process :: Matrix Bool -> [Int] -> Int -> ([Int], [Int]) -> [([Int], [Int])]
 process m a g (c, d) = (if hs == [] then [(c, d)] else [])
-  ++ (nub $ concat $ (getMinGen m (c, d) <$> fs))
+  ++ (foldr union [] $ (getMinGen m (c, d) <$> fs))
   where
-    hs = filter (< g) (c \\ a)
-    fs = filter (> g) ([1..nrows m] \\ c)
+    hs = takeWhile  (< g) (c \\ a)
+    fs = dropWhile (<= g) ([1..nrows m] \\ c)
 
 getMinGen :: Matrix Bool -> ([Int], [Int]) -> Int -> [([Int], [Int])]
 getMinGen m (c, d) f = process m z f (x, y)
